e1457173d6e16ad16492f117764ed5e587bb944d
Sethuraman Annamalai <sethuramanannamalai@Sethuramans-MacBook-Pro.local>
2018-03-10 22:20:44 -0800

    Bag of Words for Classification

---
+++ src/clean_text.py
@@ -0,0 +1,28 @@
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
+
+
+def give_clean_words_list(text):
+    name_list = []
+    tokens = nltk.tokenize.word_tokenize(text)
+    pos = nltk.pos_tag(tokens)
+    sentences = nltk.ne_chunk(pos, binary=False)
+    for subtree in sentences.subtrees():
+        if subtree.label() == 'PERSON':
+            for leaf in subtree.leaves():
+                name_list.append(leaf[0].lower())
+
+    tokens = [w.lower() for w in tokens]
+    lmtzr = WordNetLemmatizer()
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    words = [word for word in stripped if word.isalpha()]
+    stop_words = set(stopwords.words('english'))
+    clean_words = []
+    for word in words:
+        if word not in stop_words and word not in name_list:
+            clean_words.append(word)
+    clean_words = [lmtzr.lemmatize(w, 'v') for w in clean_words]
+    return clean_words
