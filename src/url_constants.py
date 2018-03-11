813ba95f285624da7daf754005fddf323917d68e
Sethuraman Annamalai <sethuramanannamalai@Sethuramans-MacBook-Pro.local>
2018-03-10 16:50:41 -0800

    URL added

--- src/url_constants.py
+++ src/url_constants.py
@@ -1,4 +1,5 @@
 good_reads_home_url = "https://www.goodreads.com"
+good_reads_isbn_to_id = "https://www.goodreads.com/book/isbn_to_id?key=DhgIAQhIi8Kij3MHkqA&isbn="
 good_reads_2000s_best_url = "https://www.goodreads.com/list/show/5.Best_Books_of_the_Decade_2000s"
 good_reads_2010s_best_url = "https://www.goodreads.com/list/show/4093.Best_Books_of_the_Decade_2010_s"
 
