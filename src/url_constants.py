6934fab0012e990b1020a1d2f4abe114df4112d9
Sethuraman Annamalai <sethuramanannamalai@Sethuramans-MacBook-Pro.local>
2018-03-13 12:06:25 -0700

    Genre dissection changes

--- src/url_constants.py
+++ src/url_constants.py
@@ -2,6 +2,6 @@ good_reads_home_url = "https://www.goodreads.com"
 good_reads_isbn_to_id = "https://www.goodreads.com/book/isbn_to_id?key=DhgIAQhIi8Kij3MHkqA&isbn="
 good_reads_2000s_best_url = "https://www.goodreads.com/list/show/5.Best_Books_of_the_Decade_2000s"
 good_reads_2010s_best_url = "https://www.goodreads.com/list/show/4093.Best_Books_of_the_Decade_2010_s"
-good_reads_explore_genre = "https://www.goodreads.com/search?search[query]=dystopia&commit=Search&search_type=books&search[field]=genre"
+good_reads_explore_genre = "https://www.goodreads.com/search?&commit=Search&search_type=books&search[field]=genre"
 
 best_books_url_list = [good_reads_2000s_best_url, good_reads_2010s_best_url]
