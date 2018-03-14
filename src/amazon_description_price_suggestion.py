0a4816f919b4a0dfd93427e170baa2b25ce3e5d4
Lakshayy Dua <lakshayy.dua@gmail.com>
2018-03-14 15:12:19 -0700

    Minor Changes

--- src/amazon_description_price_suggestion.py
+++ src/amazon_description_price_suggestion.py
@@ -11,11 +11,11 @@ import csv
 import codecs
 import numpy as np
 
-def get_amazon_price(amazon_url, browser):
+def get_amazon_price(amazon_url, selenium_instance):
 
     found_price = False
 
-    if not browser:
+    if not selenium_instance:
         browser.get(amazon_url)
 
     for i in range(10):
@@ -68,94 +68,47 @@ def get_amazon_price(amazon_url, browser):
         except:
             return 'NaN'
 
-def get_amazon_suggestions(amazon_url, total_suggestions, browser):
+def get_amazon_suggestions(amazon_url, number_of_recommendations, selenium_instance):
 
-    if not browser:
+    if not selenium_instance:
         browser.get(amazon_url)
 
-    suggestion_data =  dict()                       # Dictionary
-    number_of_suggestions = total_suggestions +1
+    recommendation_data =  list()
+    total_recommendation = number_of_recommendations + 1
 
-    for i in range(1, number_of_suggestions):
+    for i in range(1, total_recommendation):
 
         try:
-            suggestion_title = browser.find_element_by_xpath('//h2[text()="Customers who bought this item also bought"]/../../../div[2]/div/div[2]/div/ol/li['+str(i)+']/div/a/div[2]').text
-            suggestion_url = browser.find_element_by_xpath('//h2[text()="Customers who bought this item also bought"]/../../../div[2]/div/div[2]/div/ol/li['+str(i)+']/div/a').get_attribute("href")
+            recommendation_title = browser.find_element_by_xpath('//h2[text()="Customers who bought this item also bought"]/../../../div[2]/div/div[2]/div/ol/li['+str(i)+']/div/a/div[2]').text
+            recommendation_url = browser.find_element_by_xpath('//h2[text()="Customers who bought this item also bought"]/../../../div[2]/div/div[2]/div/ol/li['+str(i)+']/div/a').get_attribute("href")
 
-            suggestion_data[suggestion_title] = suggestion_url
+            recommendation_data.append(recommendation_title)
+            recommendation_data.append(recommendation_url)
 
         except:
             continue
 
-    return suggestion_data
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return recommendation_data
 
 def get_amazon_description(amazon_url, selenium_instance):
 
-    # if not selenium_instance:
-    #     browser.get(amazon_url)
+    if not selenium_instance:
+        browser.get(amazon_url)
 
     browser.switch_to.frame(browser.find_element_by_css_selector("#bookDesc_iframe"))  # To switch to iframe
 
-    desc = browser.find_element_by_xpath('//*[@id="iframeContent"]').text
+    amazon_description = browser.find_element_by_xpath('//*[@id="iframeContent"]').text
 
-
-    print(desc)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return amazon_description
 
 
 browser = webdriver.Firefox(executable_path='/home/ldua/geckodriver')
 df = pd.read_csv('../data/description.csv')
+# df_prices = pd.read_csv('../data/image_and_prices.csv')
+# df_description = pd.read_csv('../data/desc.csv')
+
+out = csv.writer(codecs.open("../data/recommendation.csv", "w", "utf-8"), delimiter=",", quoting=csv.QUOTE_ALL)
+out.writerow(['Book Title','ISBN','R1 Title','R1 URL','R2 Title','R2 URL','R3 Title','R3 URL','R4 Title','R4 URL','R5 Title','R5 URL'])
 
 for index, row in df.iterrows():
 
@@ -185,11 +138,20 @@ for index, row in df.iterrows():
             amazon_url = amazon_url.replace('amazon.fr', 'amazon.com')
             browser.get(amazon_url)
 
+    time.sleep(2)
     # amazon_price = get_amazon_price(amazon_url, True)
-    # suggestion_dict = get_amazon_suggestions(amazon_url, 5, True)
+    r_list = get_amazon_suggestions(amazon_url, 5, True) #recommendation list
     # amazon_description = get_amazon_description(amazon_url, True)
 
-
-
-
-
+    # print(amazon_price)
+    # for i in suggestion_dict.value():
+    #     print(i)
+    # print(amazon_description)
+    r_list.insert(0, row['ISBN'])
+    r_list.insert(0, row['Book Title'])
+    out.writerow(r_list)
+#     df_prices.at[index, 'Amazon Price'] = amazon_price
+#     df_descriptions.at[index, 'Amazon Price'] = amazon_price
+#     out.writerow([row['Book Title'], row['ISBN'], r_list[0], r_list[1], r_list[0], r_list[1], r_list[0], r_list[], r_list[6], r_list[7], r_list[8], r_list[9]])
+# df_prices.to_csv('../data/image_and_prices.csv')
+# ,'R2 Title','R2 URL','R3 Title','R3 URL','R4 Title','R4 URL','R5 Title','R5 URL'
