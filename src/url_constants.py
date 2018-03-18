e9c368a501bd9eec6eb4f46da38808f4c123fe63
sethuramanannamalai <Gerrardno8>
2018-03-17 20:28:10 -0700

    Changes

--- src/url_constants.py
+++ src/url_constants.py
@@ -4,9 +4,9 @@ good_reads_2000s_best_url = "https://www.goodreads.com/list/show/5.Best_Books_of
 good_reads_2010s_best_url = "https://www.goodreads.com/list/show/4093.Best_Books_of_the_Decade_2010_s"
 good_reads_explore_genre = "https://www.goodreads.com/search?&commit=Search&search_type=books&search[field]=genre"
 
-best_books_url_list = [good_reads_2000s_best_url, good_reads_2010s_best_url]
+best_books_url_list = [good_reads_2010s_best_url]
 
 
-#wiki_readgeek_description_crawler.py
+# wiki_readgeek_description_crawler.py
 google_url = 'https://www.google.com/'
 readgeek_url = 'https://www.readgeek.com/search/search?q='
